5e7a26c8eb64366653a9e0b739a5bbe9bb578e88
omargoundali <o.goundali@gmail.com>
2025-10-19 16:15:49 +0200

    Booking

---
+++ DataAccess/Models/Booking.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace DataAccess.Models
+{
+    internal class Booking
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerPhone { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Time {  get; set; }
+
+        public int Status { get; set; }
+        public int NumberOfGuests { get; set; }
+    }
+}
